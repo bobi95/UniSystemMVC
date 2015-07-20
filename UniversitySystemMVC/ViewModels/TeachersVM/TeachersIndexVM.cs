f2489ec09e716cbeff2971bdc82832ec4c563000
Anton Cholakov <anton.cholakov@programista.pro>
2015-07-20 16:53:23 +0300

    GradesFun

---
+++ UniversitySystemMVC/ViewModels/TeachersVM/TeachersIndexVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversitySystemMVC.Entity;
+
+namespace UniversitySystemMVC.ViewModels.TeachersVM
+{
+    public class TeachersIndexVM
+    {
+        public string FullName { get; set; }
+
+        public List<CoursesSubjects> CoursesSubjects { get; set; }
+    }
+}
