49f84f167c1e0dd190a15ca3884e9d13188792be
bobi95 <bobi9502@yahoo.com>
2015-07-21 12:31:40 +0300

    TemplateEngineInProgress

--- UniversitySystemMVC/Scripts/UniSystem.GradeEdit.js
+++ UniversitySystemMVC/Scripts/UniSystem.GradeEdit.js
@@ -1,41 +1,106 @@
 ﻿var UniSystem = UniSystem || {};
 
-UniSystem.GradeEdit = (function () {
+UniSystem.Templater = (function () {
     
-    var containers = [],
-
-        settings = {
-            containerClass: 'grade-crud',
-            studentIdAttribute: 'grade-student',
-            subjectIdAttribute: 'grade-subject', 
-            btnAddClass: 'add-grade-btn',
-            btnCancelClass: 'remove-grade-btn',
-            inputClass: 'add-grade-input'
-        };
-
-    var publicAPI = {
-
-        setSettings: function(newSettings) {
-            settings = newSettings;
-        },
-
-        addEvents: function () {
-
-            var containers = document.querySelectorAll(settings.containerClass);
-            
-            for (var key in containers) {
-
-                var addBtn = containers[key].querySelector(settings.btnAddClass),
-                    cancelBtn = containers[key].querySelector(settings.btnCancelClass),
-                    inputClass = containers[key].querySelector(settings.inputClass);
-
-                // TODO: add event habdlers
-            }
-
-        }
-        
+    var expressionsRAW = {
+        findCode: '(.+?(?:{{[^{}]*}}))',
+        templateCodeRaw: '[^{}]+',
+        nameRaw: '[a-z_][a-z0-9_]*',
+        objRaw: '(?:[a-z_][a-z0-9_.]*[a-z0-9_])|(?:[a-z_][a-z0-9_]*)',
+        funcPrefix: '#',
     };
 
-    return publicAPI;
+    expressionsRAW.func = expressionsRAW.funcPrefix + expressionsRAW.nameRaw;
+    expressionsRAW.funcCapture = expressionsRAW.funcPrefix + '(' + expressionsRAW.nameRaw + ')';
+
+    var expressions = {
+        findCode: new RegExp(expressionsRAW.findCode, 'i'),
+        templateCode: new RegExp('({{' + expressionsRAW.templateCodeRaw + '}})', 'g'),
+        func: new RegExp('^{{' + expressionsRAW.funcCapture + '}}$', 'i'),
+        obj: new RegExp('^{{(?:' + expressionsRAW.func + '\\s)?(' + expressionsRAW.objRaw + ')}}$', 'i')
+    };
+
+    function _compile(html) {
+        /// <summary>
+        /// Compiles the given html and returns an executable template function.
+        /// </summary>
+        /// <param name="html" type="String"></param>
+        /// <returns type="template"></returns>
+
+        var templateCode = [];
+
+
+        while (true) {
+
+            var index = html.search(expressions.templateCode);
+
+            if (index === -1) {
+
+                if (html) {
+                    templateCode.push(html);
+                }
+
+                break;
+            }
+            
+            var noncode = html.substring(0, index);
+
+            if (noncode) {
+
+                templateCode.push(noncode);
+                html = html.substring(noncode.length);
+                continue;
+
+            }
+
+            var code = html.match(expressions.templateCode);
+
+            if (code) {
+
+                templateCode.push(_processCode(code[0]));
+                continue;
+
+            }
+
+            break;
+        }
+
+        return new template(templateCode);
+    }
+
+    function _processCode(expr) {
+
+        
+
+    }
+
+    function _run(code, funcs, context) {
+        
+    }
+
+    
+
+    function template(templateCode) {
+
+        var code = templateCode,
+            funcs = {};
+
+        return {
+
+            registerFunction: function(key, func) {
+                funcs[key] = func;
+            },
+
+            run: function (context) {
+                return _run(code, funcs, context);
+            }
+        };
+    }
+
+    var templaterPublicAPI = {
+        compile: _compile
+    };
+
+    return templaterPublicAPI;
 
 })();
